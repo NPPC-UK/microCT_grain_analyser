29c92fcf6575dd640b62928fde5310916fb9db13
sirsharpest <nathan1hughes@gmail.com>
2017-07-24 12:40:15 +0100

    Added better setup file

---
+++ MATLAB/setup.m
@@ -0,0 +1,27 @@
+% This is a example file of how you should set up the running of an
+% entire experiment's data elements
+% 
+% 1. you should give a directory with a recursive file
+% path (if requried)
+%
+% 2. a size of structuring element to use for morphological
+% operations.
+%
+% 3. A voxel size used in measurements
+%
+% 4. A minimum size of interest, this allows for throwing away
+% uninteresting data
+%
+% Current values are just examples. 
+
+
+%Setup parms
+directory = '~/Data/00000079/*/*.ISQ'; 
+structEleSize = 3;
+voxelSize = 29.6;
+minSize = 7000; 
+
+tic % start timer
+% Process the file directory! 
+processDirectory(directory, structEleSize, voxelSize, minSize); 
+toc % get run time
