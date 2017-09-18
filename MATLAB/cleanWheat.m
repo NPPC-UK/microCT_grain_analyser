a6fc16fa8ba7ec827fdebb75a26d0749a82ec0c8
Nathan Hughes <nathan1hughes@gmail.com>
2017-09-19 00:28:05 +0100

    splitter update

--- MATLAB/cleanWheat.m
+++ MATLAB/cleanWheat.m
@@ -57,7 +57,7 @@ else
 end
 
 % Split up image as needed 
-%bw = watershedSplit3D(bw); 
+bw = watershedSplit3D(bw); 
 
 % Filter out any left over objects which haven't been split
 [bw, gray] = filterSmallObjs(bw, gray, minSize); 
