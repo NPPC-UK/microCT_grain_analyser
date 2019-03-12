539509c7215f9310f5f9db25ae6437237ff94789
Maximilian Friedersdorff <max@friedersdorff.com>
2019-03-12 10:14:21 +0000

    Use packbits compression on output TIFFs
    Tiffs produced are the full size of the original
    scan, but contain mostly (>90%) pixels with value 0.
    This is a perfect target for run length encoding

--- MATLAB/writeTif.m
+++ MATLAB/writeTif.m
@@ -1,8 +1,14 @@
 function writeTif(A, filename)
 % Writes a image stack A to a tif file
 delete (filename); 
-for K=1:length(A(1, 1, :))
-    imwrite(A(:, :, K), filename, 'WriteMode', 'append','Compression','none');
+imwrite(A(:, :, 1), filename, 'tiff', 'Compression', 'packbits');
+for K=2:length(A(1, 1, :))
+	try
+		imwrite(A(:, :, K), filename, 'tiff', 'WriteMode', 'append','Compression','packbits');
+	catch ME
+		fprintf(['An error occured at K: ', num2str(K), '\n']);
+		fprintf(ME.getReport);
+	end
 end
 
 end
