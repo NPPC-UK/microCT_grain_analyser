0e9fc00e4996d9248ead784932fe42f19d45d568
Maximilian Friedersdorff <max@friedersdorff.com>
2019-05-21 09:39:45 +0100

    Aggressively clear objects from memory

--- MATLAB/processDirectory.m
+++ MATLAB/processDirectory.m
@@ -39,16 +39,20 @@ for file=startFrom:endAt
         dims = calcDimensions(bw_obj);
         file_output_dims = strcat(filename, 'dims.csv');
         writetable(struct2table(dims), file_output_dims);
+	clear dims file_output_dims;
 
 	% Write rachis information and file
 	file_output_rachis = strcat(filename, '-rachis.tif');
 	writeTif(r, file_output_rachis);
+	clear r file_output_rachis;
+
         rstats = [];
         rstats.rtop = rtop;
         rstats.rbot = rbot;
         file_output_rstats = strcat(filename, '-rachis.csv');
         delete(file_output_rstats);
         writetable(struct2table(rstats), file_output_rstats);
+	clear rstats file_output_rstats;
         
         % perform grain measurement gathering!
         [stats, rawstats] = countGrain(gray, filename, voxelSize, minGrainSize);
@@ -62,19 +66,24 @@ for file=startFrom:endAt
         delete (file_output_rawstats);
         writetable(struct2table(stats), file_output_stats);
         writetable(struct2table(rawstats), file_output_rawstats);
+	clear starts rawstats file_output_stats file_output_rawstats;
 
         % Write segmented images to file
         file_output_bw = strcat(filename, 'bw-segmented.tif');
         writeTif(bw, file_output_bw);
+	clear bw file_output_bw;
 
         file_output_gray = strcat(filename, 'gray-segmented.tif');
         writeTif(gray, file_output_gray);
+	clear gray file_output_gray;
 
         file_output_bw_obj = strcat(filename, 'bw_obj-segmented.tif');
         writeTif(bw_obj, file_output_bw_obj);
+	clear bw_obj file_output_bw_obj;
 
         file_output_gray_obj = strcat(filename, 'gray_obj-segmented.tif');
         writeTif(gray_obj, file_output_gray_obj);
+	clear gray_obj file_output_gray_obj;
 
 
 end
